91c585680469439f9add1e979cb219f483ff7492
wallcrawler <ronan_mccabe@hotmail.com>
2015-06-20 13:24:01 +0100

    starting pair

---
+++ script.js
@@ -0,0 +1,99 @@
+    method: ['GET', 'POST'],
+    path: '/login',
+    config: {
+        auth: {
+          mode: "optional",
+          strategy: "google"
+        },
+        handler: handlers.loginUser
+      }
+  },
+  
+
+      loginUser: function(request, reply) {
+      request.log('analytics request is being sent');
+      if(request.auth.isAuthenticated) {
+        mandrill.sendEmail(request);
+        request.auth.session.set(request.auth.credentials.profile);
+        reply.redirect('/my-account');
+      } else
+      {
+        reply.redirect("/").code(401);
+      }
+
+    },
+
+var Hapi = require('hapi'),
+    fs = require('fs'),
+    Path = require('path'),
+    Good = require('good'),
+    Bell = require('bell'),
+    AuthCookie = require('hapi-auth-cookie'),
+    server = new Hapi.Server({debug: {request: ['error']}});
+
+server.connection({ port: process.env.PORT });
+
+// serves up views (html template pages)
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: Path.join(__dirname, "views")
+});
+
+var authOptions = {
+    provider: 'google',
+    password: 'google-encryption-password', //Password used for encryption
+    clientId: process.env.GOOGLE_CLIENT_ID,//'YourAppId',
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,//'YourAppSecret',
+    isSecure: false //means authentication can occur over http
+};
+
+//register plugins with server
+server.register(
+  [{
+    register: Good,
+    options: {
+      reporters: [{
+        reporter: require('good-http'),
+        events: { request: '*' },
+        config: {
+          endpoint : 'http://localhost:8000/analytics',
+          threshold: 0
+        }
+      }]
+    }
+  },
+  { register: Bell},
+  { register: AuthCookie}
+  ],
+
+  function (err) {
+    if (err) {
+      throw err; // something bad happened loading the plugin
+    }
+
+    server.auth.strategy("google", 'bell', authOptions);
+
+    server.auth.strategy('session', 'cookie', {
+        cookie: 'sid',
+        password: '12345678',
+        // redirectTo: '/', //this allows logout to work!
+        isSecure: false
+        // ttl: 3000  //expiry time of cookie
+        // clearInvalid: true
+    });
+
+    server.auth.default('session');  //if no auth is specified it defaults to checking the session cookie
+    server.route(require('./routes'));
+
+  }
+
+);
+
+server.start(function () {
+    server.log('Server running at: ' + server.info.uri);
+});
+
+
+module.exports = server;
