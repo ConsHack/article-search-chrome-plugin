298ebfe862f01df6a14165713557cfe1dc888c77
Anni Vaananen <anni.vaananen@gmail.com>
2015-06-20 14:15:39 +0100

    Authentication via google working

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ var Hapi = require('hapi'),
     Path = require('path'),
     Bell = require('bell'),
     AuthCookie = require('hapi-auth-cookie'),
+
     server = new Hapi.Server();
 
 server.connection({ port: process.env.PORT });
 
 // serves up views (html template pages)
-server.views({
+/*server.views({
   engines: {
     html: require('handlebars')
   },
   path: Path.join(__dirname, "views")
 });
+*/
 
 var authOptions = {
     provider: 'google',
@@ -23,22 +25,15 @@ var authOptions = {
 };
 
 //register plugins with server
-server.register(
-  [
-  { register: Bell},
-  { register: AuthCookie}
-  ],
+server.register([Bell, AuthCookie], function (err) {
+  if (err) throw err;
 
-  function (err) {
-    if (err) {
-      throw err; // something bad happened loading the plugin
-    }
 
     server.auth.strategy("google", 'bell', authOptions);
 
     server.auth.strategy('session', 'cookie', {
         cookie: 'sid',
-        password: '12345678',
+        password: process.env.COOKIE_PW,
         // redirectTo: '/', //this allows logout to work!
         isSecure: false
         // ttl: 3000  //expiry time of cookie
@@ -48,19 +43,32 @@ server.register(
     server.auth.default('session');  //if no auth is specified it defaults to checking the session cookie
     server.route({
       method: ['GET', 'POST'],
-      path: '/login',
+      path: '/',
       config: {
         auth: {
-          mode: "optional",
+          mode: "try",
           strategy: "google"
         },
         handler: function(request, reply) {
           request.log('analytics request is being sent');
           if(request.auth.isAuthenticated) {
             request.auth.session.set(request.auth.credentials.profile);
-            reply.redirect('/my-account');
+            reply.file('views/dashboard.html');
+            console.log(request.auth.credentials);
+            console.log('Is isAuthenticated: '+request.auth.isAuthenticated);
+            var creds = request.auth.credentials;
+                    
+              var profile = {
+                  googleId: creds.profile.id,
+                  fullName: creds.profile.displayName,
+                  firstName: creds.profile.name.first,
+                  email: creds.profile.email,
+                  pictures : []
+              };
+
+
           } else {
-            reply.redirect("/").code(401);
+            reply.file("views/popup.html").code(401);
           }
         }
     }
@@ -69,8 +77,7 @@ server.register(
 );
 
 server.start(function () {
-    server.log('Server running at: ' + server.info.uri);
+    console.log('Server running at: ' + server.info.uri);
 });
 
 
-module.exports = server;
